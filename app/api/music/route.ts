a89d07f88fc94e05d2173b37d2a6496371ab19fd
veronicacheng2 <95119686+veronicacheng2@users.noreply.github.com>
2023-09-06 22:33:09 -0700

    Added MusicPage

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,38 +1,41 @@
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
+import Replicate from 'replicate'
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY 
-  });
+const replicate = new Replicate({
+    auth:process.env.REPLICATE_API_TOKEN as string
+})
 
 export async function POST(req:Request){
     try{
         const {userId} = auth()
         const body = await req.json();
-        const {messages} = body
+        const {prompt} = body
+
+        console.log(prompt.prompt)
 
         if(!userId){
             return new NextResponse("Unauthorized",{status:401});
         }
 
-        if(!openai.apiKey){
-            return new NextResponse("OpenAI API Key not configured", {status:500})
+        if(!prompt){
+            return new NextResponse("Prompt is required!", {status:400})
         }
 
-        if(!messages){
-            return new NextResponse("Messages are required!", {status:400})
-        }
+        const response = await replicate.run(
+            "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+            {
+              input: {
+                prompt_a: prompt.prompt
+              }
+            }
+          );
 
-        const response = await openai.chat.completions.create({
-            model:"gpt-3.5-turbo",
-            messages
-        });
-
-        return NextResponse.json(response.choices[0].message)
+        return NextResponse.json(response)
     }
     catch(err){
-        console.log("[CONVERSATION_ERROR]",err)
+        console.log("[MUSIC_ERROR]",err)
         return new NextResponse("Internal Error",{status:500})
     }
 }
