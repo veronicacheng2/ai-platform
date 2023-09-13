48eb74e27150262a0da6396fc6edb2db55bb3cf7
veronicacheng2 <95119686+veronicacheng2@users.noreply.github.com>
2023-09-13 15:07:09 -0700

    Fix typo in landing-hero

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -7,7 +7,7 @@ import TypewriterComponent from "typewriter-effect";
 import { Button } from "./ui/button";
 
 const LandingHero = () => {
-  const { isSignedIn } = useAuth;
+  const { isSignedIn } = useAuth();
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
